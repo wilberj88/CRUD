7e56ab5479cee2693d08736d3b5b6425426e8fe9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-08 19:10:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_gsheet import GSheetsConnection
+from st-gsheets-connection import GSheetsConnection
 
 url = "https://docs.google.com/spreadsheets/d/1tW1NgWwTD-rWk8TYbA4NglQ9dwvKlm2Qrqn7nz9kULM/edit?usp=sharing"
 
