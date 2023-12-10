3b5884865d5b9601914dba2a73f590bd24dbd8e4
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-12-10 23:45:18 +0100

    Update ikimando.py

--- pages/ikimando.py
+++ pages/ikimando.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(worksheet="Users")
 
 st.header("Novus Mando 🎮 - Conexión GSheet Privada")
-st.subheader("🧠Iki❤️ 📁")
+st.subheader("🧠Iki❤️ ✍️ Inscripción")
 
+
+st.subheader("🧠Iki❤️ 📁 BD")
 st.dataframe(data)
 
 
